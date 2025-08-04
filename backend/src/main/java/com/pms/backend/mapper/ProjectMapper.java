61a4c8a4f215ce9564ce98d93e1482b921de062c
khuongloc125 <khuongloc.khl@gmail.com>
2025-08-04 17:27:46 +0700

    fix

--- backend/src/main/java/com/pms/backend/mapper/ProjectMapper.java
+++ backend/src/main/java/com/pms/backend/mapper/ProjectMapper.java
@@ -11,9 +11,10 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ProjectMapper {
+
     @Mapping(target = "created_at", expression = "java(java.time.LocalDate.now())")
-    @Mapping(target = "color", ignore = true) // Tạm thời bỏ qua, sẽ được xử lý trong service
-    @Mapping(target = "short_name", ignore = true) // Tạm thời bỏ qua, sẽ được xử lý trong service
+    @Mapping(target = "color", ignore = true)
+    @Mapping(target = "short_name", ignore = true)
     @Mapping(target = "created_by_id", ignore = true) // Được xử lý trong service
     @Mapping(target = "created_by_name", ignore = true) // Được xử lý trong service
     @Mapping(target = "leader", ignore = true) // Được xử lý trong service
@@ -22,11 +23,10 @@ public interface ProjectMapper {
     @Mapping(target = "status", constant = "ACTIVE") // Đặt trạng thái mặc định là ACTIVE
     Project toProject(ProjectCreationRequest request);
 
-    
     ProjectResponse toProjectResponse(Project project);
 
-    @Mapping(target = "project_name", source = "project_name")  
-    @Mapping(target = "description", source = "description") 
+    @Mapping(target = "project_name", source = "project_name")
+    @Mapping(target = "description", source = "description")
     @Mapping(target = "start_date", source = "start_date")
     @Mapping(target = "end_date", source = "end_date")
     @Mapping(target = "status", source = "status")
